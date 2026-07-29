cb1fc417a75863ad3ce5af08b75b18b691af1e23
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-29 21:02:05 +0530

    Update main.tf

--- Day-5-piplelinefromSCM/main.tf
+++ Day-5-piplelinefromSCM/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    ami = "ami-02b64aa047cb5edf5"
+    instance_type = "t3.micro"
     tags = {
       Name = "vvvnaresh"
     }
